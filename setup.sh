15bfabf8c13b6cb0a71920b3acb41202b7b8388e
Yuma Miyazaki <yuma.miyazaki@satori.team>
2023-04-30 16:28:36 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,18 +2,21 @@
 
 set -eu
 
-git clone git@github.com:amiyzku/nix-home-manager.git ~/.config/home-manager/
-
-# install nix
+# Install nix.
 sh <(curl -L https://nixos.org/nix/install) --daemon
 
-# apply nix && run command
+# Apply nix && run command.
 exec bash -lc "
 set -eu
 
-# install home-manager
+# Add home-manageri to channel.
 nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
 nix-channel --update
+
+# Place repo before installing home-manager.
+git clone git@github.com:amiyzku/nix-home-manager.git ~/.config/home-manager/
+
+# Install home-manager.
 nix-shell '<home-manager>' -A install
 
 # apply ~/.config/home-manager/home.nix

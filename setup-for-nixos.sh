a08afa80242e034f17cea5ed3676e1fcbdc01513
Yuma Miyazaki <yuma.miyazaki@satori.team>
2023-05-14 19:29:56 +0900

    Fix setup script for nixos

--- setup-for-nixos.sh
+++ setup-for-nixos.sh
@@ -8,23 +8,13 @@ if [[ $(uname -a | grep -i nixos) == 1 ]]; then
   exit 1
 fi
 
-if ! command -v "home-manager" &> /dev/null; then
-  echo "Error: Not found home-manager package"
-  echo ''
-  echo 'Try editing as follows:
-vim /etc/nixos/configuration.nix
-```
-users.users.<name> = {
-  packages = with pkgs; [
-    home-manager
-  ];
-};
-```
-  '
-  exit 1
-fi
-
 OUT_DIR=$HOME/.config/nixpkgs
 mkdir -p $OUT_DIR
 git clone git@github.com:amiyzku/nix-home-manager.git $OUT_DIR
+
+if ! command -v "home-manager" &> /dev/null; then
+  sudo nix-channel --add https://github.com/nix-community/home-manager/archive/release-22.11.tar.gz home-manager
+  sudo nix-channel --update
+  nix-shell '<home-manager>' -A install
+fi
 

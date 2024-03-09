41cf6176c200d73df75830a4a454df7ed45b7d67
amiyzku <amiyzku@gmail.com>
2024-03-09 19:33:15 +0900

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,6 +9,10 @@ sh <(curl -L https://nixos.org/nix/install) --daemon
 exec bash -lc "
 set -eu
 
+# Add unstable nix channel.
+nix-channel --add https://channels.nixos.org/nixpkgs-unstable nixpkgs
+nix-channel --update
+
 # Add home-manageri to channel.
 nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
 nix-channel --update
